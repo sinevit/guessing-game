2f45d64975a7ecf26b09612c73b0463cd40b2215
Kris <Krismail>
2022-08-15 15:16:36 +0300

    feat: add function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
 
+        return Math.round((this.maxValue - this.minValue)/2 +this.minValue)
     }
 
     lower() {
-
+        this.maxValue = this.guess();
     }
 
     greater() {
-
+        this.minValue = this.guess();
     }
 }
 
